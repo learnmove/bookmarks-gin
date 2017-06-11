b40a6f8bc136117b710c855608275797d5d656f2
Štefan Jarina <stefan@jarina.cz>
2017-06-11 11:15:51 +0200

    Support for more databases. Add configuration file in TOML format.

--- config.go
+++ config.go
@@ -6,9 +6,10 @@ import (
 	"path/filepath"
 
 	"github.com/codegangsta/cli"
+	"github.com/spf13/viper"
 )
 
-// KEY - Global variable for keu used by token validation
+// KEY - Global variable for key used by token validation
 var KEY string
 
 // InitKeyPath - Initialize the location of KEY
@@ -27,7 +28,7 @@ func getAddrIP(clc *cli.Context) string {
 	} else if clc.String("ip") != "" {
 		address = clc.String("ip")
 	} else {
-		address = "0.0.0.0"
+		address = viper.GetString("app.ip")
 	}
 
 	if os.Getenv("OPENSHIFT_NODEJS_PORT") != "" {
@@ -35,8 +36,32 @@ func getAddrIP(clc *cli.Context) string {
 	} else if os.Getenv("PORT") != "" {
 		port = os.Getenv("PORT")
 	} else {
-		port = "3000"
+		port = viper.GetString("app.port")
 	}
 
 	return address + ":" + port
+}
+
+func getSQLString() string {
+	var engine, host, user, password, sslmode, dbname, filepath, port string
+	// read config into variables
+	engine = viper.GetString("database.engine")
+	host = viper.GetString("database.host")
+	port = viper.GetString("database.port")
+	user = viper.GetString("database.user")
+	password = viper.GetString("database.password")
+	dbname = viper.GetString("database.dbname")
+	sslmode = viper.GetString("database.sslmode")
+	filepath = viper.GetString("database.filepath")
+
+	// build connection string for different databases
+	if engine == "mysql" {
+		return fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True", user, password, dbname)
+	} else if engine == "postgres" {
+		return fmt.Sprintf("host=%s user=%s dbname=%s sslmode=%s password=%s", host, user, dbname, sslmode, password)
+	} else if engine == "mssql" {
+		return fmt.Sprintf("sqlserver://%s:%s@%s:%s?database=%s", user, password, host, port, dbname)
+	}
+	// fallback to sqlite3
+	return filepath
 }
